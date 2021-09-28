03464b38b1b79c7d7b80126aa2efb6a1816feebc
camogrady <pboyaci@gmail.com>
2021-09-28 15:57:11 -0500

    attention_crowding part 1 instructions
    attention_crowding part 1 instructions

--- PRL Induction procedures/utilities/attention_crowding_instruction_part1.m
+++ PRL Induction procedures/utilities/attention_crowding_instruction_part1.m
@@ -1,20 +1,19 @@
-fixationlength=10;
-Screen('DrawLine', w,[255 255 255], wRect(3)/2, 3*wRect(4)/4-fixationlength, wRect(3)/2, 3*wRect(4)/4+fixationlength, 4);
-Screen('DrawLine', w,[255 255 255], wRect(3)/2-fixationlength, 3*wRect(4)/4, wRect(3)/2+fixationlength, 3*wRect(4)/4, 4);
 
+%c's
 AtheLetter=imread('newletterc22.tiff');
 
 AtheLetter=AtheLetter(:,:,1);
 
 AtheLetter=imresize(AtheLetter,0.7);
 [cc rr]=size(AtheLetter);
-imagelocation1=[100 450 165 515]; imagelocation2=[200 450 265 515]; imagelocation3=[300 450 365 515]; imagelocation4=[400 450 465 515];
-imagelocation=[(wRect(3)/2)-(3*cc)-cc/2 (3*wRect(4)/4)-cc/2 (wRect(3)/2)-(3*cc)+cc/2 (3*wRect(4)/4)+cc/2];
+imagelocation1=[100 350 165 415]; imagelocation2=[200 350 265 415]; imagelocation3=[300 350 365 415]; imagelocation4=[400 350 465 415];
 
 AtheLetter=Screen('MakeTexture', w, AtheLetter); 
 ori=90;
 Screen('DrawTexture',w,AtheLetter,[],imagelocation1,ori);Screen('DrawTexture',w,AtheLetter,[],imagelocation2,ori+180);Screen('DrawTexture',w,AtheLetter,[],imagelocation3,ori+270);Screen('DrawTexture',w,AtheLetter,[],imagelocation4,ori+90);
-Screen('DrawTexture',w,AtheLetter,[],imagelocation,ori);
-DrawFormattedText(w, 'Please keep your eyes at the center of the screen.\n \nTarget stimulus -C- can appear in four main directions: right, left, up and down.\n \nPlease indicate the direction of the gap of the C using the arrow keys as quick and accurate as possible.\n \nAs soon as you respond, you will have an auditory feedback. \n \n  \n \n \n \n Press any key to start', 100, 100, [255 255 255]);
+
+%text
+DrawFormattedText(w, 'Please keep your eyes at the center of the screen.\n \nTarget stimulus -C- can appear in four main directions: right, left, up and down.\n \nPlease indicate the direction of the gap of the C using the arrow keys as quick and accurate as possible.\n \nAs soon as you respond, you will have an auditory feedback.\n \nPossible stimuli are: \n \n \n  \n \n \n \n Press any key to start', 100, 100, [255 255 255]);
+
 Screen('Flip', w);
 KbQueueWait;
