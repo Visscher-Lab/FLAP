5a4f6c91dcc50b9f34680afb35517a694da57867
Visscher Lab <pboyaci@gmail.com>
2022-01-18 16:31:19 -0600

    add info related UAB

--- PRL Induction procedures/FLAP_contour_integration.m
+++ PRL Induction procedures/FLAP_contour_integration.m
@@ -1,15 +1,14 @@
 % Contrast threshold measurement with randomized position of the target -
 close all; clear all; clc;
 commandwindow % drawgabor drawimage GenerateEnvelope GenerateTone imageproc immagini3 phase visualcue
-station='b'; % which testing site? 'r' is for Rutgers; 's' is for the Seitz lab, 'b' is for braingamecenter
 shapeType=2; % 1 is S, 2 is open C, and 3 is closed C, 4 is dense closed
 addJitter=1; %Add or remove both types of jitter (orientation and positional)
 
-prompt={'Subject Name', 'Session'};
+prompt={'Subject Name', 'Session', 'Site'};
 
 name= 'Subject Name';
 numlines=1;
-defaultanswer={'test', ''};
+defaultanswer={'test', '','a'};
 answer=inputdlg(prompt,name,numlines,defaultanswer);
 if isempty(answer)
     return;
@@ -18,6 +17,7 @@ end;
 
 SUBJECT = answer{1,:}; %Gets Subject Name
 Session = answer{2,:}; %Gets Subject Name
+station= answer{3,:}; % which testing site? 'r' is for Rutgers; 's' is for the Seitz lab, 'b' is for braingamecenter, 'a' is for alabama
 expdayeye=Session;
 %Timing Parameters and Gamma
 
@@ -50,21 +50,30 @@ if station=='s' %Seitz lab
     screencm=[24 18];
     struct.res=[1024 768];
     struct.sz=[screencm(1), screencm(2)];
+    BITS=0; %0;
 elseif station=='r' %Rutgers
     screencm=[59.7 33.6];
     struct.res=[2560 1440];
     struct.sz=[screencm(1), screencm(2)];
+    BITS=0; %0;
 elseif station=='b'
-            screencm=[40.6 30];
-     struct.res=[1280 960];
+    screencm=[40.6 30];
+    struct.res=[1280 960];
     struct.sz=[screencm(1), screencm(2)];
- 
+    BITS=0; %0;
+elseif station == 'a'
+    %this site uses a pc, so the filenames differ.
+    baseName=[cd '\data\' SUBJECT '_' Session '_CI_' num2str(c(1)-2000) '_' num2str(c(2)) '_' num2str(c(3)) '_' num2str(c(4)) '_' num2str(c(5))]; %makes unique filename
+    screencm= [40 30]; %[69.8 35.5];
+    struct.res= [1920 1080]; %[3840 2160]; %[1920 1080];
+    struct.sz=[screencm(1), screencm(2)];
+    BITS=0; %0;
 end
 
-v_d=57;
+v_d=57;  % distance in cm to the screen [this is a guess. COMMENT YOUR CODE.]
 sigma_deg = .1; %bpk: Gaussian envelope size?
 sfs=6;
-BITS=0; %0;
+% BITS=0; %0; This is now set for 'station'
 CircConts=1;
 fixat=1;
 SOA=.1;
@@ -922,15 +931,12 @@ yeye2=[];
      [eyetime2, StimulusOnsetTime, FlipTimestamp, Missed]=Screen('Flip',w);
      
         end
-%    Screen('Flip', w);
-    
 
-    %         stim_stop=secs;
-    time_stim(trial) = stim_end - stim_start;
-    rispo(trial)=resp;
-    
-    
-Resp_RT(trial)=resp_time-stim_start;
+        time_stim(trial) = stim_end - stim_start;
+        rispo(trial)=resp;
+        
+        
+        Resp_RT(trial)=resp_time-stim_start;
      
 %     WaitSecs(.5);
 %     
@@ -1037,8 +1043,6 @@ end
         Eyelink('Shutdown');
     end;
     
-    
-           % save(baseName,'-regexp', '^(?!(wavedata|sig|tone|G|m|x|y|ax|ay|xxx|yyyy|circle|azimuths|corrS|errorS|Allimg)$).');
     save(baseName);
     
     
