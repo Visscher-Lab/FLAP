114815e488557fb0964752c0d3c19cd490d07953
MManiglia <69861583+MManiglia@users.noreply.github.com>
2022-08-10 11:03:55 -0700

    updated aca and carrasco tasks

--- PRL Induction procedures/FLAP_ACA_commented.m
+++ PRL Induction procedures/FLAP_ACA_commented.m
@@ -13,7 +13,7 @@ try
     
     name= 'Parameters';
     numlines=1;
-    defaultanswer={'test','1', '1', '1','0', '0', '4', '2' };
+    defaultanswer={'test','1', '1', '1','0', '0', '4', '2', '1' };
     answer=inputdlg(prompt,name,numlines,defaultanswer);
     if isempty(answer)
         return;
