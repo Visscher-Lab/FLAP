2dba56bd00e9ebfc42e2e1553f87459cf577a51c
pdemirayak <pboyaci@gmail.com>
2022-10-18 14:31:34 -0500

    Data

--- PRL Induction procedures/utilities/InstructionCIAssessment.m
+++ PRL Induction procedures/utilities/InstructionCIAssessment.m
@@ -2,21 +2,21 @@
 
 Screen('FillRect', w, gray);
 if shapesoftheDay(mixtr(trial,1))==1
-    if trial == 1 || mod(trial,round(length(mixtr)/16))==0 && mixtr(trial,2) == 1
+    if trial == 1 || mod(trial,round(length(mixtr)/80))==0 && mixtr(trial,2) == 1
         DrawFormattedText(w, 'Here you will see the target on the LEFT side of the scotoma \n\n Press the left arrow key if you see a d \n \n Press  the right arrow key if you see a p  \n \n \n \n Press any key to start',...
             'center','center',white);
     else
-        if mod(trial,round(length(mixtr)/16)) == 0 && mixtr(trial,2) == 2
+        if mod(trial,round(length(mixtr)/80)) == 0 && mixtr(trial,2) == 2
             DrawFormattedText(w, 'Here you will see the target on the RIGHT side of the scotoma \n\n Press the left arrow key if you see a d \n \n Press  the right arrow key if you see a p  \n \n \n \n Press any key to start',...
                 'center','center',white);
         end
     end
 elseif shapesoftheDay(mixtr(trial,1))==2
-    if mod(trial,round(length(mixtr)/16))==0 && mixtr(trial,2) == 1
+    if mod(trial,round(length(mixtr)/80))==0 && mixtr(trial,2) == 1
         DrawFormattedText(w, 'Here you will see the target on the LEFT side of the scotoma \n\n Press the left arrow key if you see a 2 \n \n Press  the right arrow key if you see a 5 \n \n \n \n Press any key to start',...
             'center','center',white);
     else
-        if mod(trial,round(length(mixtr)/16)) == 0 && mixtr(trial,2) == 2
+        if mod(trial,round(length(mixtr)/80)) == 0 && mixtr(trial,2) == 2
             DrawFormattedText(w, 'Here you will see the target on the RIGHT side of the scotoma \n\n Press the left arrow key if you see a 2 \n \n Press  the right arrow key if you see a 5  \n \n \n \n Press any key to start',...
                 'center','center',white);
         end
