6d156809e623741dc4ecb0631d74b7531e49d02c
pdemirayak <61640976+pdemirayak@users.noreply.github.com>
2022-10-31 15:13:42 -0500

    nothing to change

--- PRL Induction procedures/utilities/InstructionShapeScanner.m
+++ PRL Induction procedures/utilities/InstructionShapeScanner.m
@@ -1,20 +1,8 @@
 
 Screen('FillRect', w, gray);
-if shapesoftheDay(mixtr(trial,1))==1
-    DrawFormattedText(w, 'Press the left arrow key if you see a 9 \n \n Press  the right arrow key if you see a 6  \n \n \n \n Press any key to start', 'center', 'center', white);
-elseif shapesoftheDay(mixtr(trial,1))==2
-    DrawFormattedText(w, 'Press the left arrow key if you see a 2 \n \n Press  the right arrow key if you see a 5  \n \n \n \n Press any key to start', 'center', 'center', white);
-elseif shapesoftheDay(mixtr(trial,1))==3
-    DrawFormattedText(w, 'Press the left arrow key if you see a q \n \n Press  the right arrow key if you see a p  \n \n \n \n Press any key to start', 'center', 'center', white);
-elseif shapesoftheDay(mixtr(trial,1))==4
-    DrawFormattedText(w, 'Press the left arrow key if you see a d \n \n Press  the right arrow key if you see a b  \n \n \n \n Press any key to start', 'center', 'center', white);
-elseif shapesoftheDay(mixtr(trial,1))==5
-    DrawFormattedText(w, 'Press the left arrow key if the shape points left \n \n Press  the right arrow key if the shape points right  \n \n \n \n Press any key to start', 'center', 'center', white);
-elseif shapesoftheDay(mixtr(trial,1))==6
-    DrawFormattedText(w, 'Press the left arrow key if the line is tilted left \n \n Press  the right arrow key if the line is tilted right  \n \n \n \n Press any key to start', 'center', 'center', white);
-elseif shapesoftheDay(mixtr(trial,1))==7
-    DrawFormattedText(w, 'Press the left arrow key if the line is horizontal \n \n Press  the right arrow key if the line is vertical  \n \n \n \n Press any key to start', 'center', 'center', white);
-end
+
+    DrawFormattedText(w, 'Before each trial, a cue will indicate the location \n \n of the stimulus to attend (right: > or left: < ) \n \n Please look at the center of the screen during the experiment\n \n Index finger for 6 or left oriented stimulus \n Middle finger for 9 or right oriented stimulus \n \n','center', 100, white');
+
 
 
 %                 CIexample1=Targori{shapesoftheDay(mixtr(trial,1))}(1,:);
@@ -48,8 +36,7 @@ Screen('DrawTextures', w, TheGaborsSmall, [], imageRect_offsCIinstr2' + [example
 imageRect_offsCI2instr2(setdiff(1:length(imageRect_offsCIinstr2),exampletargetcord2),:)=0;
 
 Screen('DrawTextures', w, TheGaborsSmall, [], imageRect_offsCI2instr2'+ [examplexJitLoc2; yJitLoc; examplexJitLoc2; exampleyJitLoc2], exampletheori2,[], 0.88);
-Screen('Flip', w);
-KbQueueWait;
+FirstInstructionOnsetTime=Screen('Flip', w);
 WaitSecs(0.5);
     
     %%
