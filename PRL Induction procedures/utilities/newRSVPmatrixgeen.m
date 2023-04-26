58edc443e0b4ab3c213712a2dc80f12176406608
MManiglia <69861583+MManiglia@users.noreply.github.com>
2023-04-26 16:53:23 -0700

    SPOT aanalyses

--- PRL Induction procedures/utilities/newRSVPmatrixgeen.m
+++ PRL Induction procedures/utilities/newRSVPmatrixgeen.m
@@ -81,10 +81,6 @@ end
 
 % add incongruent trials
 
-
-
-
-
 trialype{1}=[ 3 ones(randi(3),1)'*2 3 ones(randi(4),1)'*2 3 ones(randi(4),1)'*2 4 ];
 trialype{2}=[ 3 ones(randi(3),1)'*2 3 ones(randi(8),1)'*2 3 4 ];
 trialype{3}=[ 3 ones(randi(3),1)'*2 3 ones(randi(2),1)'*2 3 ones(randi(3),1)'*2 3  ones(randi(3),1)'*2 3  ones(randi(2),1)'*2 4 ];
@@ -135,37 +131,44 @@ five=mixtr(:,1)==3 & mixtr(:,2)==1;
 six=mixtr(:,1)==3 & mixtr(:,2)==2;
 
 %identify specific switch types in the mixtr
+
+
+% add 5 incongruent trials out of 24 (20%) (the stream is on 1, the target goes to 2 but the cue is on 3)
 onef=find(one==1);
 idx=onef(randperm(length(onef),5));
-
 mixtr(idx,2)=3;
 ddd(1,:)= idx;
 
 clear idx
+% add 5 incongruent trials out of 24 (20%) (the stream is on 1, the target goes to 3 but the cue is on 2)
 
 twof=find(two==1);
 idx=twof(randperm(length(twof),5));
 mixtr(idx,2)=2;
 ddd(2,:)= idx;
 clear idx
+% add 5 incongruent trials out of 24 (20%) (the stream is on 2, the target goes to 1 but the cue is on 3)
 
 threef=find(three==1);
 idx=threef(randperm(length(threef),5));
 mixtr(idx,2)=3;
 ddd(3,:)= idx;
 clear idx
+% add 5 incongruent trials out of 24 (20%) (the stream is on 2, the target goes to 3 but the cue is on 1)
 
 fourf=find(four==1);
 idx=fourf(randperm(length(fourf),5));
 mixtr(idx,2)=1;
 ddd(4,:)= idx;
 clear idx
+% add 5 incongruent trials out of 24 (20%) (the stream is on 3, the target goes to 1 but the cue is on 2)
 
 fivef=find(five==1);
 idx=fivef(randperm(length(fivef),5));
 mixtr(idx,2)=2;
 ddd(5,:)= idx;
 clear idx
+% add 5 incongruent trials out of 24 (20%) (the stream is on 3, the target goes to 2 but the cue is on 1)
 
 sixf=find(six==1);
 idx=sixf(randperm(length(sixf),5));
@@ -175,3 +178,20 @@ clear idx
 save RSVP3mxIII.mat
 
 %randx means a rand sequence 1-x long that ends in a target (T) distractor (D) or cue (C) 
+
+congone=setdiff(onef,ddd(1,:))
+congtwo=setdiff(twof,ddd(2,:))
+congthree=setdiff(threef,ddd(3,:))
+congfour=setdiff(fourf,ddd(4,:))
+congfive=setdiff(fivef,ddd(5,:))
+congsix=setdiff(sixf,ddd(6,:))
+incongone=ddd(1,:)
+incongtwo=ddd(2,:)
+incongthree=ddd(3,:)
+incongfour=ddd(4,:)
+incongfive=ddd(5,:)
+incongsix=ddd(6,:)
+% ddd = incongruent trials. each row is a switch type. each row contains
+% the trials with the incongruent switch. these trials end with a 'wrong'
+% cue, thus the incongruent trial is the one after this
+%
