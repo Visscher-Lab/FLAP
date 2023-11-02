fdec68d357094f6bca140ea5dd93a9c4cc6c930b
sjaya012 <sjaya012@ucr.edu>
2023-11-01 22:01:19 -0700

    Correction to CI instructions and modification of first trial stimulus duration in practice

--- PRL Induction procedures/utilities/InstructionCIAssessmentPractice.m
+++ PRL Induction procedures/utilities/InstructionCIAssessmentPractice.m
@@ -1,23 +1,23 @@
 %% This script displays the instruction page for the CI Assessment Task
 
 Screen('FillRect', w, gray);
-if mixtr(trial,1)==2
-    if trial == 1 || mod(trial,trials)==1 && mixtr(trial,2) == 1
-        DrawFormattedText(w, 'Lets practice a few trials of the task. Here you will see the target on the LEFT side of the scotoma \n Press the green button if you see a q \n Press the red button if you see a p  \n Press any key to start',...
+if mixtr(trial,1)==1
+    if trial == 1 || mixtr(trial,2) == 1
+        DrawFormattedText(w, 'Here you will see the target on the LEFT side of the scotoma \n Press the green button if you see a d \n Press the red button if you see a p  \n Press any key to start',...
             'center','center',white);
     else
-        if mod(trial,trials) == 1 && mixtr(trial,2) == 2
-            DrawFormattedText(w, 'Lets practice a few trials of the task. Here you will see the target on the RIGHT side of the scotoma \n Press the green button if you see a q \n Press the red button if you see a p \n Press any key to start',...
+        if trial == 1 || mixtr(trial,2) == 2
+            DrawFormattedText(w, 'Here you will see the target on the RIGHT side of the scotoma \n Press the green button if you see a d \n Press the red button if you see a p  \n Press any key to start',...
                 'center','center',white);
         end
     end
-elseif mixtr(trial,1)==1
-    if mod(trial,trials)==1 && mixtr(trial,2) == 1
-        DrawFormattedText(w, 'Lets practice a few trials of the task. Here you will see the target on the LEFT side of the scotoma \n Press the green button if you see a d \n Press  the red button if you see a b \n Press any key to start',...
+elseif mixtr(trial,1)==2
+    if trial == 1 || mixtr(trial,2) == 1
+        DrawFormattedText(w, 'Here you will see the target on the LEFT side of the scotoma \n Press the green button if you see a left pointing egg \n Press  the red button if you see a right pointing egg \n Press any key to start',...
             'center','center',white);
     else
-        if mod(trial,trials) == 1 && mixtr(trial,2) == 2
-            DrawFormattedText(w, 'Lets practice a few trials of the task. Here you will see the target on the RIGHT side of the scotoma \n Press the green button if you see a d \n Press  the red button if you see a b \n Press any key to start',...
+        if trial == 1 || mixtr(trial,2) == 2
+            DrawFormattedText(w, 'Here you will see the target on the RIGHT side of the scotoma \n Press the green button if you see a left pointing egg \n Press the red button if you see a right pointing egg \n Press any key to start',...
                 'center','center',white);
         end
     end
