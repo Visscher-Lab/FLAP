7e6b6070a5ed4aa8066b227928c11c4c55540bbc
MManiglia <69861583+MManiglia@users.noreply.github.com>
2023-11-07 12:14:50 -0800

    updated eyefixation5

--- PRL Induction procedures/utilities/eyefixation5.m
+++ PRL Induction procedures/utilities/eyefixation5.m
@@ -22,15 +22,15 @@ elseif EyeTracker==1
         end
     elseif EyetrackerType==2
         eye_used=0;
-        [xScreenLeft, yScreenLeft,xScreenRight, yScreenRight , ~, ~, ~, ~, ~] = Datapixx('GetEyePosition');
+        [xScreenRight, yScreenRight, xScreenLeft, yScreenLeft , ~, ~, ~, ~, ~] = Datapixx('GetEyePosition');
         if whicheye==1
             pos = Datapixx('ConvertCoordSysToCustom', [xScreenLeft, yScreenLeft]);
-            [Left_Major, Left_Minor]= Datapixx('GetPupilSize');
+            [~, ~, Left_Major, Left_Minor]= Datapixx('GetPupilSize');
             newpupil_maj=Left_Major;
             newpupil_min=Left_Minor;
         elseif whicheye==2
             pos = Datapixx('ConvertCoordSysToCustom', [xScreenRight, yScreenRight]);
-            [~, ~,Right_Major, Right_Minor]= Datapixx('GetPupilSize');
+            [Right_Major, Right_Minor]= Datapixx('GetPupilSize');
             newpupil_maj=Right_Major;
             newpupil_min=Right_Minor;
         end
