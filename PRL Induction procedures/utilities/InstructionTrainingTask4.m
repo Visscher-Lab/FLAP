054a9fde16e547c38182ef0e2ff1da0fac6945e3
mgragland <mgragland@gmail.com>
2024-07-10 09:27:58 -0500

    updated instructions for rotated images

--- PRL Induction procedures/utilities/InstructionTrainingTask4.m
+++ PRL Induction procedures/utilities/InstructionTrainingTask4.m
@@ -1,6 +1,6 @@
 if trial==1 || trial== 71 || trial== 211 || trial== 351
 Screen('FillRect', w, gray);
-DrawFormattedText(w, 'Now that you have practiced, we will begin the task. \n \n Using your eyes, move the white circle over the dot until the dot starts flickering. \n \n When the dot changes into a tilted blob or a shape, \n \n press the left (green) or right (red) button \n \n to indicate the orientation of the tilted blob or shape   \n \n \n Press any key to start', 'center', 'center', white);
+DrawFormattedText(w, 'Now that you have practiced, we will begin the task. \n \n Using your eyes, move the white circle over the dot until the dot starts flickering. \n \n When the dot changes into a tilted blob or a shape, \n \n press the left/green or right/red button \n \n to indicate the orientation of the tilted blob or shape   \n \n \n Press any key to start', 'center', 'center', white);
 Screen('Flip', w);
 KbQueueWait;
 end
@@ -11,7 +11,7 @@ end
 %% Contrast Task
 if trial == 1
     if mixtr(trial,3) == 1
-        DrawFormattedText(w, 'Press the left (green) or right (red) button \n \n to indicate the orientation of the tilted blob \n \n Press any key to start', 'center', 'center', white);
+          DrawFormattedText(w, 'Press the left/green or right/red button \n \n to indicate the orientation of the tilted blob \n \n Press any key to start', 'center', 'center', white);
         ori1 = -45; ori2 = 45;
         Ypos=PRLx*pix_deg_vert;
         Xpos=PRLx*pix_deg;
@@ -28,37 +28,37 @@ if trial == 1
         
     elseif mixtr(trial,3) == 2
         if shapesoftheDay(mixtr(trial,1))==1
-            DrawFormattedText(w, 'Press the green button if you see a d \n \n Press  the red button if you see a p  \n \n Press any key to start', 'center', 'center', white);
+            DrawFormattedText(w, 'Press the left/green button if you see a d \n \n Press  the right/red button if you see a p  \n \n Press any key to start', 'center', 'center', white);
         elseif shapesoftheDay(mixtr(trial,1))==2
-            DrawFormattedText(w, 'Press the green button if you see a q \n \n Press  the red button if you see a b  \n \n Press any key to start', 'center', 'center', white);
+            DrawFormattedText(w, 'Press the left/green button if you see a q \n \n Press  the right/red button if you see a b  \n \n Press any key to start', 'center', 'center', white);
         elseif shapesoftheDay(mixtr(trial,1))==3
-            DrawFormattedText(w, 'Press the green button if you see a q \n \n Press  the red button if you see a p  \n \n Press any key to start', 'center', 'center', white);
+            DrawFormattedText(w, 'Press the left/green button if you see a q \n \n Press  the right/red button if you see a p  \n \n Press any key to start', 'center', 'center', white);
         elseif shapesoftheDay(mixtr(trial,1))==4
-            DrawFormattedText(w, 'Press the green button if you see a d \n \n Press  the red button if you see a b  \n \n Press any key to start', 'center', 'center', white);
+            DrawFormattedText(w, 'Press the left/green button if you see a d \n \n Press  the right/red button if you see a b  \n \n Press any key to start', 'center', 'center', white);
         elseif shapesoftheDay(mixtr(trial,1))==5
-            DrawFormattedText(w, 'Press the green button if you see a 9 \n \n Press  the red button if you see a 6  \n \n Press any key to start', 'center', 'center', white);
+            DrawFormattedText(w, 'Press the left/green button if you see a 9 \n \n Press  the right/red button if you see a 6  \n \n Press any key to start', 'center', 'center', white);
         elseif shapesoftheDay(mixtr(trial,1))==6
-            DrawFormattedText(w, 'Press the green button if you see a 2 \n \n Press  the red button if you see a 5  \n \n Press any key to start', 'center', 'center', white);
+            DrawFormattedText(w, 'Press the left/green button if you see a 2 \n \n Press  the right/red button if you see a 5  \n \n Press any key to start', 'center', 'center', white);
         elseif shapesoftheDay(mixtr(trial,1))==7
-            DrawFormattedText(w, 'Press the green button if you see an egg pointing left \n \n Press  the red button if you see an egg pointing right  \n \n Press any key to start', 'center', 'center', white);
+            DrawFormattedText(w, 'Press the left/green button if you see an egg pointing left \n \n Press  the right/red button if you see an egg pointing right  \n \n Press any key to start', 'center', 'center', white);
         elseif shapesoftheDay(mixtr(trial,1))==8
-            DrawFormattedText(w, 'Press the green button if you see a horizontal line \n \n Press  the red button if you see a vertical line  \n \n Press any key to start', 'center', 'center', white);
+            DrawFormattedText(w, 'Press the left/green button if you see a horizontal line \n \n Press  the right/red button if you see a vertical line  \n \n Press any key to start', 'center', 'center', white);
         elseif shapesoftheDay(mixtr(trial,1))==9
-            DrawFormattedText(w, 'Press the green button if you see a sleeping d \n \n Press  the red button if you see a sleeping p  \n \n Press any key to start', 'center', 'center', white);
+            DrawFormattedText(w, 'Press the left/green button if you see a sleeping d (line on top) \n \n Press  the right/red button if you see a sleeping p (line on bottom) \n \n Press any key to start', 'center', 'center', white);
         elseif shapesoftheDay(mixtr(trial,1))==10
-            DrawFormattedText(w, 'Press the green button if you see a sleeping q \n \n Press  the red button if you see a sleeping b  \n \n Press any key to start', 'center', 'center', white);
+            DrawFormattedText(w, 'Press the left/green button if you see a sleeping q (line on top) \n \n Press  the right/red button if you see a sleeping b (line on bottom)  \n \n Press any key to start', 'center', 'center', white);
         elseif shapesoftheDay(mixtr(trial,1))==11
-            DrawFormattedText(w, 'Press the green button if you see a sleeping q \n \n Press  the red button if you see a sleeping p  \n \n Press any key to start', 'center', 'center', white);
+            DrawFormattedText(w, 'Press the left/green button if you see a sleeping q (line on top) \n \n Press  the right/red button if you see a sleeping p (line on bottom) \n \n Press any key to start', 'center', 'center', white);
         elseif shapesoftheDay(mixtr(trial,1))==12
-            DrawFormattedText(w, 'Press the green button if you see a sleeping d \n \n Press  the red button if you see a sleeping b  \n \n Press any key to start', 'center', 'center', white);
+            DrawFormattedText(w, 'Press the left/green button if you see a sleeping d (line on top) \n \n Press  the right/red button if you see a sleeping b (line on bottom) \n \n Press any key to start', 'center', 'center', white);
         elseif shapesoftheDay(mixtr(trial,1))==13
-            DrawFormattedText(w, 'Press the green button if you see a sleeping 9 \n \n Press  the red button if you see a sleeping 6  \n \n Press any key to start', 'center', 'center', white);
+            DrawFormattedText(w, 'Press the left/green button if you see a sleeping 9 \n \n Press  the right/red button if you see a sleeping 6 \n \n Press any key to start', 'center', 'center', white);
         elseif shapesoftheDay(mixtr(trial,1))==14
-            DrawFormattedText(w, 'Press the green button if you see a slanted 2 \n \n Press  the red button if you see a slanted 5 \n \n Press any key to start', 'center', 'center', white);
+            DrawFormattedText(w, 'Press the left/green button if you see a slanted 2 \n \n Press  the right/red button if you see a slanted 5 \n \n Press any key to start', 'center', 'center', white);
         elseif shapesoftheDay(mixtr(trial,1))==15
-            DrawFormattedText(w, 'Press the green button if you see a downward pointing egg \n \n Press  the red button if you see an upward pointing egg \n \n Press any key to start', 'center', 'center', white);
+            DrawFormattedText(w, 'Press the left/green button if you see a downward pointing egg \n \n Press the right/red button if you see an upward pointing egg \n \n Press any key to start', 'center', 'center', white);
         elseif shapesoftheDay(mixtr(trial,1))==16
-            DrawFormattedText(w, 'Press the green button if you see a line tilted left \n \n Press  the red button if you see a line tilted right  \n \n Press any key to start', 'center', 'center', white);
+            DrawFormattedText(w, 'Press the left/green button if you see a line tilted left \n \n Press the right/red button if you see a line tilted right  \n \n Press any key to start', 'center', 'center', white);
         end
         theeccentricity_X = 7*pix_deg;
         if TRLlocation == 1
@@ -117,7 +117,7 @@ if trial == 1
     end
 else
     if mixtr(temptrial+1,3) == 1
-        DrawFormattedText(w, 'Press the green (left) or red (right) button \n \n to indicate the orientation of the pattern \n \n Press any key to continue', 'center', 'center', white);
+        DrawFormattedText(w, 'Press the left/green or right/red button \n \n to indicate the orientation of the pattern \n \n Press any key to continue', 'center', 'center', white);
         ori1 = -45; ori2 = 45;
         Ypos=PRLx*pix_deg_vert;
         Xpos=PRLx*pix_deg;
@@ -129,38 +129,38 @@ else
         KbQueueWait;
         WaitSecs(0.5);
     elseif mixtr(temptrial+1,3) == 2
-        if shapesoftheDay(mixtr(temptrial+1,1))==1
-            DrawFormattedText(w, 'Press the green button if you see a d \n \n Press  the red button if you see a p  \n \n Press any key to start', 'center', 'center', white);
-        elseif shapesoftheDay(mixtr(temptrial+1,1))==2
-            DrawFormattedText(w, 'Press the green button if you see a q \n \n Press  the red button if you see a b  \n \n Press any key to start', 'center', 'center', white);
-        elseif shapesoftheDay(mixtr(temptrial+1,1))==3
-            DrawFormattedText(w, 'Press the green button if you see a q \n \n Press  the red button if you see a p  \n \n Press any key to start', 'center', 'center', white);
-        elseif shapesoftheDay(mixtr(temptrial+1,1))==4
-            DrawFormattedText(w, 'Press the green button if you see a d \n \n Press  the red button if you see a b  \n \n Press any key to start', 'center', 'center', white);
-        elseif shapesoftheDay(mixtr(temptrial+1,1))==5
-            DrawFormattedText(w, 'Press the green button if you see a 9 \n \n Press  the red button if you see a 6  \n \n Press any key to start', 'center', 'center', white);
-        elseif shapesoftheDay(mixtr(temptrial+1,1))==6
-            DrawFormattedText(w, 'Press the green button if you see a 2 \n \n Press  the red button if you see a 5  \n \n Press any key to start', 'center', 'center', white);
-        elseif shapesoftheDay(mixtr(temptrial+1,1))==7
-            DrawFormattedText(w, 'Press the green button if you see an egg pointing left \n \n Press  the red button if you see an egg pointing right  \n \n Press any key to start', 'center', 'center', white);
-        elseif shapesoftheDay(mixtr(temptrial+1,1))==8
-            DrawFormattedText(w, 'Press the green button if you see a horizontal line \n \n Press  the red button if you see a vertical line  \n \n Press any key to start', 'center', 'center', white);
-        elseif shapesoftheDay(mixtr(temptrial+1,1))==9
-            DrawFormattedText(w, 'Press the green button if you see a sleeping d \n \n Press  the red button if you see a sleeping p  \n \n Press any key to start', 'center', 'center', white);
-        elseif shapesoftheDay(mixtr(temptrial+1,1))==10
-            DrawFormattedText(w, 'Press the green button if you see a sleeping q \n \n Press  the red button if you see a sleeping b  \n \n Press any key to start', 'center', 'center', white);
-        elseif shapesoftheDay(mixtr(temptrial+1,1))==11
-            DrawFormattedText(w, 'Press the green button if you see a sleeping q \n \n Press  the red button if you see a sleeping p  \n \n Press any key to start', 'center', 'center', white);
-        elseif shapesoftheDay(mixtr(temptrial+1,1))==12
-            DrawFormattedText(w, 'Press the green button if you see a sleeping d \n \n Press  the red button if you see a sleeping b  \n \n Press any key to start', 'center', 'center', white);
-        elseif shapesoftheDay(mixtr(temptrial+1,1))==13
-            DrawFormattedText(w, 'Press the green button if you see a sleeping 9 \n \n Press  the red button if you see a sleeping 6  \n \n Press any key to start', 'center', 'center', white);
-        elseif shapesoftheDay(mixtr(temptrial+1,1))==14
-            DrawFormattedText(w, 'Press the green button if you see a slanted 2 \n \n Press  the red button if you see a slanted 5 \n \n Press any key to start', 'center', 'center', white);
-        elseif shapesoftheDay(mixtr(temptrial+1,1))==15
-            DrawFormattedText(w, 'Press the green button if you see a downward pointing egg \n \n Press  the red button if you see an upward pointing egg \n \n Press any key to start', 'center', 'center', white);
-        elseif shapesoftheDay(mixtr(temptrial+1,1))==16
-            DrawFormattedText(w, 'Press the green button if you see a line tilted left \n \n Press  the red button if you see a line tilted right  \n \n Press any key to start', 'center', 'center', white);
+      if shapesoftheDay(mixtr(trial,1))==1
+            DrawFormattedText(w, 'Press the left/green button if you see a d \n \n Press  the right/red button if you see a p  \n \n Press any key to start', 'center', 'center', white);
+        elseif shapesoftheDay(mixtr(trial,1))==2
+            DrawFormattedText(w, 'Press the left/green button if you see a q \n \n Press  the right/red button if you see a b  \n \n Press any key to start', 'center', 'center', white);
+        elseif shapesoftheDay(mixtr(trial,1))==3
+            DrawFormattedText(w, 'Press the left/green button if you see a q \n \n Press  the right/red button if you see a p  \n \n Press any key to start', 'center', 'center', white);
+        elseif shapesoftheDay(mixtr(trial,1))==4
+            DrawFormattedText(w, 'Press the left/green button if you see a d \n \n Press  the right/red button if you see a b  \n \n Press any key to start', 'center', 'center', white);
+        elseif shapesoftheDay(mixtr(trial,1))==5
+            DrawFormattedText(w, 'Press the left/green button if you see a 9 \n \n Press  the right/red button if you see a 6  \n \n Press any key to start', 'center', 'center', white);
+        elseif shapesoftheDay(mixtr(trial,1))==6
+            DrawFormattedText(w, 'Press the left/green button if you see a 2 \n \n Press  the right/red button if you see a 5  \n \n Press any key to start', 'center', 'center', white);
+        elseif shapesoftheDay(mixtr(trial,1))==7
+            DrawFormattedText(w, 'Press the left/green button if you see an egg pointing left \n \n Press  the right/red button if you see an egg pointing right  \n \n Press any key to start', 'center', 'center', white);
+        elseif shapesoftheDay(mixtr(trial,1))==8
+            DrawFormattedText(w, 'Press the left/green button if you see a horizontal line \n \n Press  the right/red button if you see a vertical line  \n \n Press any key to start', 'center', 'center', white);
+        elseif shapesoftheDay(mixtr(trial,1))==9
+            DrawFormattedText(w, 'Press the left/green button if you see a sleeping d (line on top) \n \n Press  the right/red button if you see a sleeping p (line on bottom) \n \n Press any key to start', 'center', 'center', white);
+        elseif shapesoftheDay(mixtr(trial,1))==10
+            DrawFormattedText(w, 'Press the left/green button if you see a sleeping q (line on top) \n \n Press  the right/red button if you see a sleeping b (line on bottom)  \n \n Press any key to start', 'center', 'center', white);
+        elseif shapesoftheDay(mixtr(trial,1))==11
+            DrawFormattedText(w, 'Press the left/green button if you see a sleeping q (line on top) \n \n Press  the right/red button if you see a sleeping p (line on bottom) \n \n Press any key to start', 'center', 'center', white);
+        elseif shapesoftheDay(mixtr(trial,1))==12
+            DrawFormattedText(w, 'Press the left/green button if you see a sleeping d (line on top) \n \n Press  the right/red button if you see a sleeping b (line on bottom) \n \n Press any key to start', 'center', 'center', white);
+        elseif shapesoftheDay(mixtr(trial,1))==13
+            DrawFormattedText(w, 'Press the left/green button if you see a sleeping 9 \n \n Press  the right/red button if you see a sleeping 6 \n \n Press any key to start', 'center', 'center', white);
+        elseif shapesoftheDay(mixtr(trial,1))==14
+            DrawFormattedText(w, 'Press the left/green button if you see a slanted 2 \n \n Press  the right/red button if you see a slanted 5 \n \n Press any key to start', 'center', 'center', white);
+        elseif shapesoftheDay(mixtr(trial,1))==15
+            DrawFormattedText(w, 'Press the left/green button if you see a downward pointing egg \n \n Press the right/red button if you see an upward pointing egg \n \n Press any key to start', 'center', 'center', white);
+        elseif shapesoftheDay(mixtr(trial,1))==16
+            DrawFormattedText(w, 'Press the left/green button if you see a line tilted left \n \n Press the right/red button if you see a line tilted right  \n \n Press any key to start', 'center', 'center', white);
         end
         
         theeccentricity_X = 7*pix_deg;
